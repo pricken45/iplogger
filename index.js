8c30e3810ba166b21da4446750606b60b931a6e2
Ruben <68506854+pricken45@users.noreply.github.com>
2022-06-15 19:58:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let latestIP = "";
+
+app.use(express.static("public"));
+
+app.get('/ip', (req, res) => {
+    res.json({ ip: latestIP });
+});
+
+io.on("connection", (socket) => {
+    console.log("connection")
+    latestIP = socket.handshake.address;
+});
+
+server.listen(3000);
